871767dc6ff8beb361807a1a81cce0690511dc94
DermisZabala <dermiszabala@gmail.com>
2025-05-20 14:15:00 -0400

    Add whitenoise for static files and update settings

--- cine_plataforma/settings.py
+++ cine_plataforma/settings.py
@@ -127,6 +127,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
